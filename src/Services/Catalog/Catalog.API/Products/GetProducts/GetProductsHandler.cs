1eefa8c348732dcabc64fb692a15d2c2b99f0c18
Tom <tomastomek@seznam.cz>
2024-10-17 21:53:23 +0200

    GetProducts

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    // ILogger parameter is name we want to appear in logs for this item
+    internal class GetProductsQueryHandler
+        (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            // @Query - tells logging framework to serialize query object using ToString(), but also
+            // include additional metadata about the object such as its property values
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+            // Query returns IQueryable<T> object
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
