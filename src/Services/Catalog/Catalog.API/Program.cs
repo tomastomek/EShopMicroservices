2f1b7628042ba427146594213583618e7be25f01
Tom <tomastomek@seznam.cz>
2024-10-18 19:15:29 +0200

    Seeding CatalogDb with Marten Initial Baseline Data (IInitialData interface)

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -27,6 +27,11 @@ builder.Services.AddMarten(opt =>
     opt.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
