398e4f0e63f7f6b5218214bbadedac08b257826f
Tom <tomastomek@seznam.cz>
2024-10-18 19:54:40 +0200

    Health checks

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,4 @@
-﻿using Marten.Pagination;
-
-namespace Catalog.API.Products.GetProducts
+﻿namespace Catalog.API.Products.GetProducts
 {
     public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10)
         : IQuery<GetProductsResult>;
