4ba4efa7f0d42e9f797bf6e89c4e77d2364360ba
Tom <tomastomek@seznam.cz>
2024-10-18 19:33:53 +0200

    Pagination using ToPagedListAsync

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,9 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using Marten.Pagination;
+
+namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10)
+        : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
 
     // ILogger parameter is name we want to appear in logs for this item
@@ -11,7 +14,8 @@
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
             // Query returns IQueryable<T> object
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
             return new GetProductsResult(products);
         }
