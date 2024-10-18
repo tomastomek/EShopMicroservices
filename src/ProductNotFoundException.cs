b17f880de34cec81621662664488591d0fa0b2c6
Tom <tomastomek@seznam.cz>
2024-10-18 11:55:26 +0200

    Get Product by id

---
+++ src/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException() : base("Product not found!")
+    {
+        
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProductById
+{
+    // we dont't have request object, we get id information from request parameters
+    // public record GetProductByIdRequest(Guid id)
+    public record GetProductByIdResponse(Product Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            // id comes from request parameter
+            app.MapGet("products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+
+                var response = result.Adapt<GetProductByIdResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products By Id")
+            .WithDescription("Get Products By Id");
+        }
+    }
+}
