362b8e490c79ed9dc62cdcbbf152eb8e756d1ed8
Tom <tomastomek@seznam.cz>
2024-10-21 15:58:53 +0200

    Basket.API - Get, Store, Delete using repository

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidar : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandValidar()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    internal class StoreBasketCommandHandler(IBasketRepository repository)
+        : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle
+            (StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            var  storedCart = await repository.StoreBasket(command.Cart, cancellationToken);
+            //TODO: update cache
+
+            return new StoreBasketResult(command.Cart.UserName);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog(assemblies: typeof(Program).Assembly));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    // set username as identity field
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.Run();
