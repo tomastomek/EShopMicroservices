355feceaa56928b90639113c8f3f23a4bc28b842
tomastomek <tomastomek@seznam.cz>
2024-11-01 22:50:41 +0100

    DDD - abstractions + domain modeling

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    // we need mediatr package, because we want to inherit from INotification
+    // to perform domain events
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        // which class threw this event
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Ordering.Domain.Models
+{
+    // order entities id type is guid
+    public class Order : Aggregate<Guid>
+    {
+        private readonly List<OrderItem> _orderItems = new List<OrderItem>();
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public Guid CustomerId { get; private set; } = default!;
+        public string OrderName { get; private set; } = default!;
+        public Address ShippingAddress { get; private set; } = default!;
+        public Address BillingAddress { get; private set; } = default!;
+        public Payment Payment { get; private set; } = default!;
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+        public decimal TotalPrice
+        {
+            get => OrderItems.Sum(x => x.Price * x.Quantity);
+            private set { }
+        }
+    }
+}
