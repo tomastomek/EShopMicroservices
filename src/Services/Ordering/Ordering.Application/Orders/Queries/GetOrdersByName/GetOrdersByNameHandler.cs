e290ba437e71e870f2478a5eac5303d02ab86d88
tomastomek <tomastomek@seznam.cz>
2024-11-02 23:50:07 +0100

    Queries + handlers, pagination

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    public class GetOrdersByNameQueryHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+    {
+        public async Task<GetOrdersByNameResult> Handle
+            (GetOrdersByNameQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking() // improve performance when reading only
+                .Where(o => o.OrderName.Value.Contains(query.Name))
+                .OrderBy(o => o.OrderName)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersByNameResult(orders.ToOrderDtoList());
+        }
+    }
+}
