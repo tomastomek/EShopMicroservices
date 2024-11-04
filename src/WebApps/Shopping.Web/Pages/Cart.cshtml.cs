b3cd145d5c0d2739e26e1b6a47219aa52691f186
tomastomek <tomastomek@seznam.cz>
2024-11-04 15:02:13 +0100

    Product list, Cart

--- src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace Shopping.Web.Pages
 {
-    public class CartModel : PageModel
+    public class CartModel(IBasketService basketService, ILogger<CartModel> logger)
+        : PageModel
     {
-        public void OnGet()
+        public ShoppingCartModel Cart { get; set; } = new ShoppingCartModel();
+
+        public async Task<IActionResult> OnGetAsync()
         {
+            Cart = await basketService.LoadUserBasket();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveFromCartAsync(Guid productId)
+        {
+            logger.LogInformation("Remove from cart button clicked");
+            Cart = await basketService.LoadUserBasket();
+
+            Cart.Items.RemoveAll(x => x.ProductId == productId);
+
+            await basketService.StoreBasket(new StoreBasketRequest(Cart));
+
+            // redirect to current page
+            return RedirectToPage();
         }
     }
 }
