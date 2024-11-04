ad6cdbeb02f83d3338b89a8399869e384f2d4223
tomastomek <tomastomek@seznam.cz>
2024-11-04 12:44:40 +0100

    Web App folder structure + models

---
+++ src/WebApps/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Shopping.Web.Models.Basket;
+
+public class BasketCheckoutModel
+{
+    public string Username { get; set; } = default!;
+    public Guid CustomerId { get; set; } = default!;
+    public decimal TotalPrice { get; set; } = default!;
+    
+    // Shipping and Billing addresses
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string AddressLine { get; set; } = default!;
+    public string Country { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+    
+    // Payment
+    public string CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
+    public string CVV { get; set; } = default!;
+    public int PaymentMethod { get; set; } = default!;
+}
+
+// wrapper classes - we need to define same parameter names as in endpoints
+public record CheckoutBasketRequest(BasketCheckoutModel BasketCheckoutDto);
+public record CheckoutBasketResponse(bool IsSuccess);
---
+++ src/WebApps/Shopping.Web/Models/Ordering/PaginatedResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Web.Models.Ordering;
+
+public class PaginatedResult<TEntity>(int pageIndex, int pageSize, long count, IEnumerable<TEntity> data)
+{
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get; } = pageSize;
+    public long Count { get; } = count;
+    public IEnumerable<TEntity> Entities { get; } = data;
+}
